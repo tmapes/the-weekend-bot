b0623536e24b3f11a77ee9bbb90b48b5c98c5146
Taylor Mapes <Taylor.Mapes@target.com>
2021-05-31 16:43:14 -0500

    Update the main file

--- src/main/kotlin/the/weekend/bot/Application.kt
+++ src/main/kotlin/the/weekend/bot/Application.kt
@@ -1,10 +1,11 @@
 package the.weekend.bot
 
-import io.micronaut.runtime.Micronaut.*
+import io.micronaut.runtime.Micronaut.build
+
 fun main(args: Array<String>) {
-	build()
-	    .args(*args)
-		.packages("the.weekend.bot")
-		.banner(false)
-		.start()
+    build()
+        .args(*args)
+        .packages("the.weekend.bot")
+        .banner(false)
+        .start()
 }
