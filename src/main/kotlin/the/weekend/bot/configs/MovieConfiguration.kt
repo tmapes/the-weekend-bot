e2dc3bb60b6769e4569a807ea36a2002feac5dfd
Taylor Mapes <taylor@mapes.info>
2021-11-25 14:24:34 -0600

    Cleanup imports

--- src/main/kotlin/the/weekend/bot/configs/MovieConfiguration.kt
+++ src/main/kotlin/the/weekend/bot/configs/MovieConfiguration.kt
@@ -1,11 +1,9 @@
 package the.weekend.bot.configs
 
 import io.micronaut.context.annotation.ConfigurationInject
-import io.micronaut.context.annotation.EachBean
 import io.micronaut.context.annotation.EachProperty
 import java.time.Duration
 
-// @ConfigurationProperties("movies")
 @EachProperty("movies", list = true)
 data class MovieConfiguration @ConfigurationInject constructor(
     val title: String,
