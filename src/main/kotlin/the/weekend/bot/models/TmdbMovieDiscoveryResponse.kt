dcd152b2804e02ad75e76c5c0449c1b451d09270
Taylor Mapes <taylor@mapes.info>
2021-12-17 19:02:39 -0600

    No more snake case by default

--- src/main/kotlin/the/weekend/bot/models/TmdbMovieDiscoveryResponse.kt
+++ src/main/kotlin/the/weekend/bot/models/TmdbMovieDiscoveryResponse.kt
@@ -1,17 +1,22 @@
 package the.weekend.bot.models
 
 import com.fasterxml.jackson.annotation.JsonFormat
+import com.fasterxml.jackson.databind.PropertyNamingStrategies
+import com.fasterxml.jackson.databind.annotation.JsonNaming
 import java.time.LocalDate
 
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy::class)
 data class TmdbMovieDiscoveryResponse(
     val results: List<TmdbDiscoveredMovie>
 )
 
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy::class)
 data class TmdbDiscoveredMovie(
     val id: Int,
     @JsonFormat(pattern = "yyyy-MM-dd") val releaseDate: LocalDate
 )
 
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy::class)
 data class TmdbMovie(
     val title: String,
     val runtime: Long,
