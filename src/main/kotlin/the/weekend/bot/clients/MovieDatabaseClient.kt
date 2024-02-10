59c1fd24df0a0327daaf8c03d986e97bf40a3cad
Taylor Mapes <taylor@mapes.info>
2024-02-09 19:49:12 -0600

    - Set max page from TMDB responses. - Move away from CompletableFuture for HTTP calls. - Add client filter for the api calls

--- src/main/kotlin/the/weekend/bot/clients/MovieDatabaseClient.kt
+++ src/main/kotlin/the/weekend/bot/clients/MovieDatabaseClient.kt
@@ -7,23 +7,20 @@ import io.micronaut.http.annotation.QueryValue
 import io.micronaut.http.client.annotation.Client
 import the.weekend.bot.models.TmdbMovie
 import the.weekend.bot.models.TmdbMovieDiscoveryResponse
-import java.util.concurrent.CompletableFuture
 
 @Client("the-movie-database")
 interface MovieDatabaseClient {
 
     @Get("/3/movie/top_rated")
-    fun topRatedMovies(
-        @QueryValue("api_key") apiKey: String,
+    suspend fun topRatedMovies(
         @QueryValue("page") page: Int,
         @QueryValue("language") language: String = "en-US",
         @QueryValue("region") region: String = "US",
-    ): CompletableFuture<HttpResponse<TmdbMovieDiscoveryResponse>>
+    ): TmdbMovieDiscoveryResponse
 
     @Get("/3/movie/{movie_id}")
-    fun getMovieById(
+    suspend fun getMovieById(
         @PathVariable("movie_id") movieId: Int,
         @QueryValue("append_to_response") appendToResponse: Set<String> = setOf("credits", "images"),
-        @QueryValue("api_key") apiKey: String
-    ): CompletableFuture<HttpResponse<TmdbMovie>>
+    ): HttpResponse<TmdbMovie>
 }
